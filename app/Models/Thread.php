4f48fbf3fbe5de2430bc875249c24a6d3302cbbb
alireza-kahdooee <alireza.kahdooee@hotmail.com>
2021-10-08 00:04:25 +0330

    Create Moddels.

---
+++ app/Models/Thread.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Thread extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['title', 'slug', 'content', 'publish', 'user_id', 'channel_id', 'answer_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function channel()
+    {
+        return $this->belongsTo(Channel::class);
+    }
+
+    public function answers()
+    {
+        return $this->hasMany(Answer::class);
+    }
+}
